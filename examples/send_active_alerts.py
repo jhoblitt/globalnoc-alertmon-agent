734bf547dd1f9df233357616405204635160b4ac
Joshua Hoblitt <josh@hoblitt.com>
2024-01-12 11:43:49 -0700

    (WIP) pull alerts from prometheus

--- examples/send_active_alerts.py
+++ examples/send_active_alerts.py
@@ -1,23 +1,36 @@
+#!/usr/bin/python3
+
 import sys
 
 import os
 import yaml
-
+import requests
 
 from globalnoc_alertmon_agent import AlertMonAgent, Alert
-    
+
 script_dir = os.path.dirname(os.path.abspath(sys.argv[0]))
-    
 config_file = script_dir+"/conf/config.yaml"
-alerts_file = script_dir+"/data/send_active_alerts.yaml"
 
 # read in config file
 with open(config_file, "r") as stream:
     config = yaml.safe_load(stream)
 
-def main():
+def sanitize_severity(severity):
+    s = 'Unknown'
 
-    # initialize the alertmon agent 
+    # match statement doesn't seem to have an easy way to do case insensitive matching, so we force everything to lower case.
+    match severity.lower():
+        case 'critical': s = 'Critical'
+        case 'major' | 'warning': s ='Major'
+        case 'minor' | 'info': s = 'Minor'
+        case 'unknown': s = 'Unknown'
+        case 'ok': s ='Ok'
+
+    print(f'severity: {severity}, s: {s}')
+    return s
+
+def main():
+    # initialize the alertmon agent
     agent = AlertMonAgent(
         username    = config.get('username'),
         password    = config.get('password'),
@@ -28,22 +41,27 @@ def main():
     alerts = get_active_alerts()
 
     for alert in alerts:
-        
+        data = {
+            'node_name': alert['labels'].get('pod', 'Unknown'),
+            'service_name': alert['labels'].get('alertname', 'Unknown'),
+            'severity': sanitize_severity(alert['labels'].get('severity', 'Unknown')),
+            'description': alert['annotations'].get('description', 'Unknown'),
+            'start_time': alert.get("activeAt", None),
+        }
+
         agent.add_alert(Alert(
-            start_time   = alert.get('start_time', None),
-            node_name    = alert.get('node_name'),
-            service_name = alert.get('service_name'),
-            description  = alert.get('description'),
-            severity     = alert.get('severity')
+            start_time   = data.get('start_time', None),
+            node_name    = data.get('node_name'),
+            service_name = data.get('service_name'),
+            description  = data.get('description'),
+            severity     = data.get('severity')
         ))
 
     agent.send_alerts()
 
 
 def get_active_alerts():
-    with open(alerts_file, "r") as stream:
-        alerts = yaml.safe_load(stream)
-
-    return alerts
+    r = requests.get('https://prometheus.ayekan.ls.lsst.org/api/v1/alerts')
+    return r.json()['data']['alerts']
 
 main()
